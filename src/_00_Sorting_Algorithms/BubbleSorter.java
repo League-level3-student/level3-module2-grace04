54f8ebad47904a67d086c5737af4332b2c830685
BuildTools <unconfigured@null.spigotmc.org>
2020-05-29 16:57:20 -0700

    bubble quick merge?

--- src/_00_Sorting_Algorithms/BubbleSorter.java
+++ src/_00_Sorting_Algorithms/BubbleSorter.java
@@ -4,17 +4,15 @@ public class BubbleSorter extends Sorter{
 	public BubbleSorter() {
 		type = "Bubble";
 	}
-	
 	//1. Use the bubble sorting algorithm to sort the array.
 	//   You can use display.updateDisplay() to show the current
 	//   progress on the graph.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-		int temp;
 		for(int j=0;j<array.length;j++) {
 			for(int i=0;i<array.length-1;i++) {
 				if(array[i]>array[i+1]) {
-					temp = array[i];
+					int temp = array[i];
 					array[i] = array[i+1];
 					array[i+1] = temp;
 					display.updateDisplay();
